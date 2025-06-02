dd79a6c9f4b9c6cac3b41f4e5f6c0b1b763d073c
Shilpa-Manaji <manajishilpa@gmail.com>
2025-06-02 11:51:52 +0530

    Updated Model_loader

--- API/model_loader.py
+++ API/model_loader.py
@@ -1,14 +1,19 @@
 import os
-from tensorflow.keras.models import load_model
 import joblib
+from tensorflow.keras.models import load_model
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_DIR = os.path.normpath(os.path.join(BASE_DIR, '..', 'Models'))
 
-def load_model_and_tokenizer():
+def load_sentiment_model_and_vectorizer():
     model_path = os.path.join(MODEL_DIR, 'final_lstm_model.h5')
-    tokenizer_path = os.path.join(MODEL_DIR, 'tokenizer.pkl')
-
+    vectorizer_path = os.path.join(MODEL_DIR, 'tokenizer.pkl')
     model = load_model(model_path)
-    tokenizer = joblib.load(tokenizer_path)
-    return model, tokenizer
+    vectorizer = joblib.load(vectorizer_path)
+    return model, vectorizer
+
+def load_recommendation_model():
+    class DummyRecommender:
+        def recommend(self, user_id):
+            return ["book1", "book2", "book3"]
+    return DummyRecommender()
